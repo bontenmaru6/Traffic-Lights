71600b1d2547059a41f7d707ba5063b4460d6368
bontenmaru6 <46227303+bontenmaru6@users.noreply.github.com>
2018-12-30 03:01:38 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,42 +1,36 @@
 (function() {
   'use strict';
 
-var blue = document.getElementById("blue");
-var yellow = document.getElementById("yellow");
-var red = document.getElementById("red");
-var change = document.getElementById("change");
+  var blue = document.getElementById("blue");
+  var yellow = document.getElementById("yellow");
+  var red = document.getElementById("red");
+  var change = document.getElementById("change");
+  var colors = [blue, yellow, red];
+  var n = 0;
 
-var colors = [blue, yellow, red];
+  change.addEventListener("click", function() {
+    if (n < 3) {
+      n += 1;
+    }
+    switch(colors[n - 1]) {
+      case blue:
+      blue.style.backgroundColor = "#66ff66";
+      yellow.style.backgroundColor = "#222222";
+      red.style.backgroundColor = "#222222";
+      break;
 
-var n = 0;
+      case yellow:
+      blue.style.backgroundColor = "#222222";
+      yellow.style.backgroundColor = "#ffff66";
+      red.style.backgroundColor = "#222222";
+      break;
 
-
-change.addEventListener("click", function() {
-  if (n < 3) {
-    n += 1;
-  }
-
-  switch(colors[n - 1]) {
-    case blue:
-    blue.className = "blue";
-    yellow.className = "light";
-    red.className = "light";
-    break;
-
-    case yellow:
-    blue.className = "light";
-    yellow.className = "yellow";
-    red.className = "light";
-    break;
-
-    case red:
-    blue.className = "light";
-    yellow.className = "light";
-    red.className = "red";
-    n = 0;
-    break;
-  }
-
-});
-
+      case red:
+      blue.style.backgroundColor = "#222222";
+      yellow.style.backgroundColor = "#222222";
+      red.style.backgroundColor = "#ff3333";
+      n = 0;
+      break;
+    }
+  });
 })();
