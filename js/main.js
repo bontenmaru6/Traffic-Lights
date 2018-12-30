0eaacc95432e473c43395728f2fdfe6136539510
bontenmaru6 <46227303+bontenmaru6@users.noreply.github.com>
2018-12-30 21:53:44 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,33 +1,37 @@
 (function() {
   'use strict'
 
-  let blue = document.getElementById("blue");
-  let yellow = document.getElementById("yellow");
-  let red = document.getElementById("red");
-  let auto = document.getElementById("auto");
-  let change = document.getElementById("change");
-  let colors = [blue, yellow, red];
+  const blue = document.getElementById("blue");
+  const yellow = document.getElementById("yellow");
+  const red = document.getElementById("red");
+  const auto = document.getElementById("auto");
+  const change = document.getElementById("change");
+  const colors = [blue, yellow, red];
   let n = 0;
 
-//クリックしたら自動で色が変化
+// クリックしたら自動で色が変化
   auto.addEventListener("click", function() {
     let count = 0;
-    let countUp = function() {
-      console.log(count++);
-      let id = setTimeout(countUp,1000);
+    const countUp = function() {
+    count++;
+    const id = setTimeout(countUp,1000);
 
-      if (count === 1) {
+    switch (count) {
+      case 1:
         blue.style.backgroundColor = "#66ff66";
         yellow.style.backgroundColor = "#222222";
         red.style.backgroundColor = "#222222";
-      } else if (count === 2) {
+        break;
+      case 2:
         blue.style.backgroundColor = "#222222";
         yellow.style.backgroundColor = "#ffff66";
         red.style.backgroundColor = "#222222";
-      }else {
+        break;
+      case 3:
         blue.style.backgroundColor = "#222222";
         yellow.style.backgroundColor = "#222222";
         red.style.backgroundColor = "#ff3333";
+        break;
       }
 
       if (count > 2) {
@@ -37,7 +41,7 @@
     countUp();
   });
 
-//クリックしたら色が一つ変化
+// クリックしたら色が一つ変化
   change.addEventListener("click", function() {
 
     switch(colors[n++]) {
