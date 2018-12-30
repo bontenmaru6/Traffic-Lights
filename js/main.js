e5a9de19c9d62972fa602b617b07fff890bdf032
bontenmaru6 <46227303+bontenmaru6@users.noreply.github.com>
2018-12-30 14:04:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,36 +1,34 @@
 (function() {
   'use strict';
 
-  var blue = document.getElementById("blue");
-  var yellow = document.getElementById("yellow");
-  var red = document.getElementById("red");
-  var change = document.getElementById("change");
-  var colors = [blue, yellow, red];
-  var n = 0;
+  let blue = document.getElementById("blue");
+  let yellow = document.getElementById("yellow");
+  let red = document.getElementById("red");
+  let change = document.getElementById("change");
+  let colors = [blue, yellow, red];
+  let n = 0;
 
   change.addEventListener("click", function() {
-    if (n < 3) {
-      n += 1;
-    }
-    switch(colors[n - 1]) {
+
+    switch(colors[n++]) {
       case blue:
-      blue.style.backgroundColor = "#66ff66";
-      yellow.style.backgroundColor = "#222222";
-      red.style.backgroundColor = "#222222";
-      break;
+        blue.style.backgroundColor = "#66ff66";
+        yellow.style.backgroundColor = "#222222";
+        red.style.backgroundColor = "#222222";
+        break;
 
       case yellow:
-      blue.style.backgroundColor = "#222222";
-      yellow.style.backgroundColor = "#ffff66";
-      red.style.backgroundColor = "#222222";
-      break;
+        blue.style.backgroundColor = "#222222";
+        yellow.style.backgroundColor = "#ffff66";
+        red.style.backgroundColor = "#222222";
+        break;
 
       case red:
-      blue.style.backgroundColor = "#222222";
-      yellow.style.backgroundColor = "#222222";
-      red.style.backgroundColor = "#ff3333";
-      n = 0;
-      break;
+        blue.style.backgroundColor = "#222222";
+        yellow.style.backgroundColor = "#222222";
+        red.style.backgroundColor = "#ff3333";
+        n = 0;
+        break;
     }
   });
 })();
