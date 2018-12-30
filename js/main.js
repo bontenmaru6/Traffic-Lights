bb35de75e2579891db3db531b2d272fda91645c5
bontenmaru6 <46227303+bontenmaru6@users.noreply.github.com>
2018-12-31 00:21:14 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,35 +11,41 @@
 
 // クリックしたら自動で色が変化
   auto.addEventListener("click", function() {
-    let count = 0;
-    const countUp = function() {
-    count++;
-    const id = setTimeout(countUp,1000);
+      let count = 0;
+      const countUp = function() {
+      count++;
+      const id = setTimeout(countUp,1000);
 
-    switch (count) {
-      case 1:
-        blue.style.backgroundColor = "#66ff66";
+      switch (count) {
+        case 1:
+          blue.style.backgroundColor = "#66ff66";
+          yellow.style.backgroundColor = "#222222";
+          red.style.backgroundColor = "#222222";
+          break;
+        case 2:
+          blue.style.backgroundColor = "#222222";
+          yellow.style.backgroundColor = "#ffff66";
+          red.style.backgroundColor = "#222222";
+          break;
+        case 3:
+          blue.style.backgroundColor = "#222222";
+          yellow.style.backgroundColor = "#222222";
+          red.style.backgroundColor = "#ff3333";
+          break;
+
+        case 4:
+        blue.style.backgroundColor = "#222222";
         yellow.style.backgroundColor = "#222222";
         red.style.backgroundColor = "#222222";
         break;
-      case 2:
-        blue.style.backgroundColor = "#222222";
-        yellow.style.backgroundColor = "#ffff66";
-        red.style.backgroundColor = "#222222";
-        break;
-      case 3:
-        blue.style.backgroundColor = "#222222";
-        yellow.style.backgroundColor = "#222222";
-        red.style.backgroundColor = "#ff3333";
-        break;
-      }
+        }
 
-      if (count > 2) {
-        clearTimeout(id);
+        if (count > 3) {
+          clearTimeout(id);
+        }
       }
-    }
-    countUp();
-  });
+      countUp();
+    });
 
 // クリックしたら色が一つ変化
   change.addEventListener("click", function() {
@@ -65,5 +71,4 @@
         break;
     }
   });
-
 })();
